0b061b74dfd4ae8f4fb6d48a7faeef3f9e859f3a
Sergey Donyk <doncarlos@i.ua>
2023-03-30 02:15:14 +0300

    addLIb_Btn

---
+++ src/js/libraryBtn.js
@@ -0,0 +1,7 @@
+// FT-14 За натисканням на кнопку "Watched" показуються переглянуті фільми користувача
+// FT-15 За натисканням на кнопку "Queue" показуються фільми додані в чергу користувача
+const refs = {
+  watched: document.querySelector('.watched'),
+  queue: document.querySelector('.queue'),
+  watchedContainer: document.querySelector('.watchedContainer'),
+};
