09c14c1c282f56cb2dbba9216c3d4e468186e0f4
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-04-01 01:06:23 +0300

    modal demo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { getPopularFilms } from './js/api';
 import { renderFilmList } from './js/renderFilmList';
 import {getSearchMovie, onSearch, addMovie } from './js/search';
+import { clickOnFilmCard } from './js/film-modal';
 
 async function addItem() {
   let data = await getPopularFilms();
