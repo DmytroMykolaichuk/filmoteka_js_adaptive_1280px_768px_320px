f2c7d484fae763845767ceff261fbba95c3ab08b
Vadym4e <vadym.o92@gmail.com>
2023-04-01 19:02:39 +0300

    after_finalization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { getPopularFilms } from './js/api';
 import { renderFilmList } from './js/renderFilmList';
-import {getSearchMovie, onSearch, addMovie } from './js/search';
+import { getSearchMovie, onSearch, addMovie } from './js/search';
 import { clickOnFilmCard } from './js/film-modal';
-
+import { playVideoTrailer, getVideoInfo } from './js/trailer';
 export async function addItem() {
   let data = await getPopularFilms();
   renderFilmList(data);
---
+++ src/js/trailer.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
+
+const basicLightbox = require('basiclightbox');
+// const button = document.querySelector('.film__trailer__btn');
+const modal = document.querySelector('.modal-container');
+
+export function getVideoInfo(movieId) {
+  return axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=352708f90836dd2b75b209ae082e91df`
+    )
+    .then(response => {
+      const videoKey = response.data.results[0].key;
+      return videoKey;
+    })
+    .catch(error => {
+      console.error('Ошибка получения информации о видео:', error);
+      throw error;
+    });
+}
+
+export const playVideoTrailer = modal.addEventListener('click', event => {
+  const button = document.querySelector('.trailer-Btn');
+
+  if (event.target.matches('.trailer-Btn')) {
+    const movieId = button.dataset.id;
+    getVideoInfo(movieId)
+      .then(videoKey => {
+        const videoHtml = `
+      <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/${videoKey}"
+        frameborder="0" 
+        allow="autoplay; encrypted-media" 
+        allowfullscreen>
+      </iframe>
+    `;
+        const istance = basicLightbox.create(videoHtml);
+        istance.show();
+      })
+      .catch(error => {
+        console.error('Ошибка:', error);
+      });
+  }
+});
