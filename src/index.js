0ea8fb2add90044739cc5b814d080ef9f60b8a37
Vadym4e <vadym.o92@gmail.com>
2023-04-01 17:15:06 +0300

    filter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { getPopularFilms } from './js/api';
 import { renderFilmList } from './js/renderFilmList';
-import {getSearchMovie, onSearch, addMovie } from './js/search';
+import { getSearchMovie, onSearch, addMovie } from './js/search';
 import { clickOnFilmCard } from './js/film-modal';
+import { playVideoTrailer, getVideoInfo } from './js/trailer';
 
 async function addItem() {
   let data = await getPopularFilms();
