227b2ffa4041e34b2813e48357cc09811f024979
Sergey Donyk <doncarlos@i.ua>
2023-04-03 01:50:50 +0300

    addAnimation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { renderFilmList } from './js/renderFilmList';
 import { getSearchMovie, onSearch, addMovie } from './js/search';
 import { clickOnFilmCard } from './js/film-modal';
 import { playVideoTrailer, getVideoInfo } from './js/trailer';
-import { onClickInGanre } from './js/choise-ganre'
-
+import { onClickInGanre } from './js/choise-ganre';
+import { funcAnimeLogo, funcAnimeHeart } from './js/SVG-animation';
 
 export async function addItem() {
   let data = await getPopularFilms();
   renderFilmList(data);
 }
+
 addItem();
