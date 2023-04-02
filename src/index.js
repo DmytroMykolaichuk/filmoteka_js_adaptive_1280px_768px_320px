2cb5edfe094cc937fb0d074731b89e15dc4c8af8
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-04-02 14:04:36 +0300

    3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { renderFilmList } from './js/renderFilmList';
 import { getSearchMovie, onSearch, addMovie } from './js/search';
 import { clickOnFilmCard } from './js/film-modal';
 import { playVideoTrailer, getVideoInfo } from './js/trailer';
+import { onClickInGanre } from './js/choise-ganre'
+
 export async function addItem() {
   let data = await getPopularFilms();
   renderFilmList(data);
