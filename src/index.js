c5378be90f7ab0405ff33ef5a5102be8b1a701cc
Oleh Protasevych <protasevicholeg@gmail.com>
2023-04-02 13:54:43 +0300

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// src/index.html
 import { getPopularFilms } from './js/api';
 import { renderFilmList } from './js/renderFilmList';
 import { getSearchMovie, onSearch, addMovie } from './js/search';
 import { clickOnFilmCard } from './js/film-modal';
 import { playVideoTrailer, getVideoInfo } from './js/trailer';
+
 export async function addItem() {
   let data = await getPopularFilms();
   renderFilmList(data);
 }
-
 addItem();
