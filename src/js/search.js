cf4bf365393504e0949ff05fa9842e861b475a9c
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-04-03 17:25:55 +0300

    choose genre

--- src/js/search.js
+++ src/js/search.js
@@ -2,18 +2,14 @@
 import axios from 'axios';
 import { renderFilmList } from './renderFilmList';
 import { addItem } from '../index';
-
+import { showPreloader, hidePreloader } from './loader';
 // import { initSearchPagination, updateSearchFilmList } from './pagination';
 // import { ITEMS_PER_PAGE } from '..';
-
-
-import { showPreloader, hidePreloader } from './loader';
 
 const gallery = document.querySelector('.film-list');
 const searchFormEl = document.querySelector('.header__movie-search-form');
 const inputEl = document.querySelector('.movie-search-form');
 const errorEl = document.querySelector('.search-error-message');
-
 const KEY = '352708f90836dd2b75b209ae082e91df';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
@@ -21,9 +17,7 @@ const instance = axios.create({
 let name;
 let page = 1;
 let pages;
-
 export let isSearchActive = false;
-
 export async function getSearchMovie(name, page) {
   try {
     const { data } = await instance.get(
@@ -36,25 +30,20 @@ export async function getSearchMovie(name, page) {
 }
 
 searchFormEl.addEventListener('submit', onSearch);
-
 export function onSearch(event) {
   event.preventDefault();
   gallery.innerHTML = '';
   name = inputEl.value.trim();
   page = 1;
-  
   if (name !== '') {
     errorEl.classList.add('visually-hidden');
     page = 1;
-
     isSearchActive = true;
     addSearchedMovie(name, page).then(() => {
       isSearchActive = false;
-
     showPreloader();
     addSearchedMovie().then(() => {
       hidePreloader();
-
     });
   });
   } else {
@@ -70,16 +59,16 @@ export async function addSearchedMovie(name, page) {
   if (searchResult.results.length !== 0) {
     errorEl.classList.add('visually-hidden');
 
-    const totalPages = searchResult.total_pages;
-    const totalItems = totalPages * 20; // 20 results per page
-    initSearchPagination(totalItems, name);
+    // const totalPages = searchResult.total_pages;
+    // const totalItems = totalPages * 20; // 20 results per page
+    // initSearchPagination(totalItems, name);
 
     // Отримуємо перші ITEMS_PER_PAGE елементів
-    const limitedResults = searchResult.results.slice(0, ITEMS_PER_PAGE);
-    renderFilmList({ ...searchResult, results: limitedResults });
+    // const limitedResults = searchResult.results.slice(0, ITEMS_PER_PAGE);
+    // renderFilmList({ ...searchResult, results: limitedResults });
 
-    console.log(searchResult);
-    console.log(pages);
+    // console.log(searchResult);
+    // console.log(pages);
 
     pages = searchResult.total_pages;
     renderFilmList(searchResult);
