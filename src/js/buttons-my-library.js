19f24ec60b2b248202a9458416c1dd9cc3edd77e
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-04-04 03:56:18 +0300

    start fix my lib

--- src/js/buttons-my-library.js
+++ src/js/buttons-my-library.js
@@ -1,31 +1,39 @@
-import { onCardWatch } from "./my-library"
+import { onCardWatch, onCardQueue } from "./my-library"
 
 const queueBtn= document.querySelector('.secondary')
 const watchedBtn= document.querySelector('.lib-btn.current')
 const btnClearAll = document.querySelector('.btn-clear-all')
-const mylibWrapper = document.querySelector('.mylib-wrapper')
+const mylibWrapper = document.querySelector('.film-list')
 
+watchedBtn.addEventListener('click', onClickWatchedBtn)
 queueBtn.addEventListener('click', onClickQueueBtn)
+btnClearAll.addEventListener('click', onClickClearAll)
+
+
+export function onClickWatchedBtn(){
+  if(watchedBtn.classList.contains('current')){return}
+  // mylibWrapper.innerHTML=''
+  queueBtn.classList.toggle('current')
+  watchedBtn.classList.toggle('current')
+  onCardWatch()
+}
+
+
 export function onClickQueueBtn(){
   if(queueBtn.classList.contains('current')){return}
   mylibWrapper.innerHTML=''
   queueBtn.classList.toggle('current')
   watchedBtn.classList.toggle('current')
 }
-watchedBtn.addEventListener('click', onClickWatchedBtn)
-export function onClickWatchedBtn(){
-  if(watchedBtn.classList.contains('current')){return}
-  mylibWrapper.innerHTML=''
-  queueBtn.classList.toggle('current')
-  watchedBtn.classList.toggle('current')
-  onCardWatch()
-}
 
-btnClearAll.addEventListener('click', onClickClearAll)
+
 export function onClickClearAll(){
   if(watchedBtn.classList.contains('current')){
     localStorage.removeItem('wathced')
     mylibWrapper.innerHTML=''
+    location.replace(location.href)
   return}
   localStorage.removeItem('queue')
+  mylibWrapper.innerHTML=''
+  location.replace(location.href)
 }
--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,42 +1,56 @@
 import axios from 'axios';
-import {
-  onClickWatchedBtn,
-  onClickQueueBtn,
-  onClickClearAll,
-} from './buttons-my-library';
+import {onClickWatchedBtn, onClickQueueBtn, onClickClearAll } from './buttons-my-library';
 import { genres } from './genres';
 import { clickOnFilmCard } from './film-modal';
 import { getVideoInfo } from './trailer';
 import { showPreloader, hidePreloader } from './loader';
-// import { showPreloader, hidePreloader } from './loader';
-// https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+
+const wraperMyLib = document.querySelector('.empty-wrapper')
 const containerListWatchedCard = document.querySelector('.film-list');
 const dataWatchedCards = JSON.parse(localStorage.getItem('wathced'));
 const dataQueueCards = JSON.parse(localStorage.getItem('queue'));
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = '352708f90836dd2b75b209ae082e91df';
 
-const queueButton = document.getElementById('queue');
-queueButton.addEventListener('click', () => {});
+// const queueButton = document.getElementById('queue');
 
+function wraper(){
+  const test = JSON.parse(localStorage.getItem('wathced'))
+  console.log(test)
+  if( localStorage.getItem('wathced') || test[0]){
+    wraperMyLib.style.display='none'
+    onCardWatch();
+    return
+  }
+  wraperMyLib.style.display='flex'
+}
+wraper()
+
+async function getWatchedFilms(page) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}${page}?api_key=${API_KEY}&language=en-US`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export async function onCardWatch() {
   containerListWatchedCard.innerHTML = '';
+  let markup = '';
   for (const idFilm of dataWatchedCards) {
     const oneFilmCard = await getWatchedFilms(idFilm);
-
     let release = Number.parseInt(oneFilmCard.release_date || oneFilmCard.first_air_date);
     const mainPoster = `https://image.tmdb.org/t/p/w300${oneFilmCard.poster_path}`;
     const posterFake = `https://shop-cdn1.vigbo.tech/shops/48947/products/18863233/images/2-be392e7cfe9a0fa843b29c1e22be8909.jpg`;
     let genreMarkup = [];
 
-    genres.forEach(genre => {
-      if (oneFilmCard.genres.includes(genre.id)) {
-        genreMarkup.push(genre.name);
-      }
+    oneFilmCard.genres.forEach(element => {
+      genreMarkup.push(element.name)
     });
 
-    let markup = '';
     markup += ` <li class ="film-item">
                 <a id='${oneFilmCard.id}' class="film-card" href="#">
                   <div class='thumb'>
@@ -51,12 +65,8 @@ export async function onCardWatch() {
                     <div class='film-data'>
                       <h2 class="title-film">${oneFilmCard.title}</h2>
                       <p>
-                         <span class='info-film'>${genreMarkup.join(
-                           ', '
-                         )} | ${release}</span>
-                        <span class ="rating">IMDB:<br>${oneFilmCard.vote_average.toFixed(
-                          1
-                        )}</span>
+                        <span class='info-film'>${genreMarkup.join(', ')} | ${release}</span>
+                        <span class ="rating">IMDB:<br>${oneFilmCard.vote_average.toFixed(1)}</span>
                       </p>
                     </div>
                   </a>
@@ -64,64 +74,43 @@ export async function onCardWatch() {
     containerListWatchedCard.insertAdjacentHTML('beforeend', markup);
   }
 }
-onCardWatch();
-//////////addEventListener//////
-queueButton.addEventListener('click', () => {
-  async function onCardQueue() {
-    containerListWatchedCard.innerHTML = '';
-    for (const idFilm of dataQueueCards) {
-      const oneFilmCard = await getWatchedFilms(idFilm);
-      let release = Number.parseInt(
-        oneFilmCard.release_date || oneFilmCard.first_air_date
-      );
-      const mainPoster = `https://image.tmdb.org/t/p/w300${oneFilmCard.poster_path}`;
-      const posterFake = `https://shop-cdn1.vigbo.tech/shops/48947/products/18863233/images/2-be392e7cfe9a0fa843b29c1e22be8909.jpg`;
-      let genreMarkup = [];
-      genres.forEach(genre => {
-        if (oneFilmCard.genres.includes(genre.id)) {
-          genreMarkup.push(genre.name);
-        }
-      });
-      let markup = '';
-      markup += ` <li class ="film-item">
-                <a id='${oneFilmCard.id}' class="film-card" href="#">
-                  <div class='thumb'>
-                  <img
-                      class = 'poster'
-                      src= "${
-                        oneFilmCard.poster_path ? mainPoster : posterFake
-                      }"
-                      alt="${oneFilmCard.title}"
-                      loading="lazy"
-                    /></div>
-                    <div class='film-data'>
-                      <h2 class="title-film">${oneFilmCard.title}</h2>
-                      <p>
-                         <span class='info-film'>${genreMarkup.join(
-                           ', '
-                         )} | ${release}</span>
-                        <span class ="rating">IMDB:<br>${oneFilmCard.vote_average.toFixed(
-                          1
-                        )}</span>
-                      </p>
-                    </div>
-                  </a>
-              </li>`;
-      console.log(markup);
-      console.log(oneFilmCard);
-      containerListWatchedCard.insertAdjacentHTML('beforeend', markup);
-    }
-  }
-});
 
-onCardQueue();
-async function getWatchedFilms(page) {
-  try {
-    const { data } = await axios.get(
-      `${BASE_URL}${page}?api_key=${API_KEY}&language=en-US`
-    );
-    return data;
-  } catch (error) {
-    console.log(error);
+
+export async function onCardQueue() {
+  containerListWatchedCard.innerHTML = '';
+  let markup = '';
+  for (const idFilm of dataQueueCards) {
+    const oneFilmCard = await getWatchedFilms(idFilm);
+    let release = Number.parseInt(oneFilmCard.release_date || oneFilmCard.first_air_date);
+    const mainPoster = `https://image.tmdb.org/t/p/w300${oneFilmCard.poster_path}`;
+    const posterFake = `https://shop-cdn1.vigbo.tech/shops/48947/products/18863233/images/2-be392e7cfe9a0fa843b29c1e22be8909.jpg`;
+    let genreMarkup = [];
+    oneFilmCard.genres.forEach(element => {
+      genreMarkup.push(element.name)
+    });
+    
+    markup += ` <li class ="film-item">
+              <a id='${oneFilmCard.id}' class="film-card" href="#">
+                <div class='thumb'>
+                <img
+                    class = 'poster'
+                    src= "${oneFilmCard.poster_path ? mainPoster : posterFake}"
+                    alt="${oneFilmCard.title}"
+                    loading="lazy"
+                  /></div>
+                  <div class='film-data'>
+                    <h2 class="title-film">${oneFilmCard.title}</h2>
+                    <p>
+                      <span class='info-film'>${genreMarkup.join(', ')} | ${release}</span>
+                      <span class ="rating">IMDB:<br>${oneFilmCard.vote_average.toFixed(1)}</span>
+                    </p>
+                  </div>
+                </a>
+            </li>`;
+    containerListWatchedCard.insertAdjacentHTML('beforeend', markup);
   }
 }
+// onCardQueue();
+
+
+
